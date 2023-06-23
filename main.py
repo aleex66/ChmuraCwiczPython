389745a7c5043c1de495130e5bda65d57b9ae2c8
aleex66 <olisia.olcia@gmail.com>
2023-06-23 23:09:06 +0200

    dddd

--- main.py
+++ main.py
@@ -6,25 +6,27 @@ from flask import render_template
 from flask import abort, url_for, make_response
 
 app = Flask(__name__)
-'''mail = Mail(app)
+'''
+mail = Mail(app)
 template = Template('Hello {{ name }}!')
 template.render(name='Aleex S')
-app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] ='da3bed477b6fa7'
+app.config['MAIL_USERNAME'] = 'da3bed477b6fa7'
 app.config['MAIL_PASSWORD'] = 'cc0f75eed05411'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
+
 @app.route("/")
 def index():
-    msg = Message('Hello', sender = 'da3bed477b6fa7', recipients = ['olisia.olcia@gmail.com'])
+    msg = Message('Hello', sender='da3bed477b6fa7', recipients=['olisia.olcia@gmail.com'])
     msg.body = "Hello Flask message sent from Flask-Mail. Odbiór żołnierzu."
+    msg.html = "<b>Hey </b>sending <a href='https://google.com'>Flask app</a>, ldk if it works"
     mail.send(msg)
     return "Sent!"
-    '''
-
+'''
 
 @app.route('/')
 def index():
